2569851a82d914cc9bf52772c65042549eae8a5c
ssster32 <is0309fs26001501878@gmail.com>
2026-06-01 14:40:09 +0900

    Add build script with chmod

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -o errexit
+
+# 1. ライブラリのインストール
+pip install -r requirements.txt
+
+# 2. 静的ファイルの収集
+python manage.py collectstatic --no-input
+
+# 3. データベースのマイグレーション
+python manage.py migrate
+
+# 4. スーパーユーザーの自動作成（環境変数がある場合のみ）
+if [[ -n "$DJANGO_SUPERUSER_USERNAME" ]]; then
+python manage.py createsuperuser --noinput || echo "Already exists."
